0400d5a2b63d0a67ea2092de524355f293c29a08
Ernst Stolz <eric@spiritsdontfly.com>
2024-03-28 08:55:03 +0100

    add packages and logic

--- app/page.js
+++ app/page.js
@@ -1,113 +1,139 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react'
+import clsx from 'clsx'
+import Squirrel from '@/public/squirrel-lol.png'
+
+const initialJokes = [
+  {
+    id: generateRandomString(),
+    content:
+      'An electron walks into a bar and asks, how much for a beer. The bartender says, for you, free of charge.',
+    highlight: 'free of charge.',
+  },
+  {
+    id: generateRandomString(),
+    content:
+      'Wie viele Programmierer sind nötig, um eine kaputte Glühbirne auszuwechseln? - Keine. Das ist ein Hardwareproblem.',
+    highlight: 'Hardwareproblem.',
+  },
+  {
+    id: generateRandomString(),
+    content: '— Ты кто по профессии? — Блогер. — Я тоже ничего не умею ...',
+    highlight: 'ничего не умею ...',
+  },
+]
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(2)
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [jokes, setJokes] = useState(initialJokes)
+  const [joke, setJoke] = useState({
+    id: generateRandomString(),
+    content: '',
+    highlight: '',
+  })
+
+  function MakeMeBeautiful({ content, highlight }) {
+    const emojis = ['😭', '😁', '😍', '😆']
+
+    if (MakeMeBeautiful.current === undefined) MakeMeBeautiful.current = 0
+    else MakeMeBeautiful.current = ++MakeMeBeautiful.current % 4
+
+    const start = content.indexOf(highlight)
+    const before = content.substring(0, start)
+    const after = content.substring(start + highlight.length)
+
+    return (
+      <p
+        key={generateRandomString()}
+        className="relative rounded-md bg-slate-600 p-2"
+      >
+        {before}
+        {highlight && (
+          <span
+            className={clsx('inline-block -rotate-2 rounded-sm p-2', {
+              'bg-amber-500': MakeMeBeautiful.current === 0,
+              'bg-sky-500': MakeMeBeautiful.current === 1,
+              'bg-green-500': MakeMeBeautiful.current === 2,
+              'bg-teal-500': MakeMeBeautiful.current === 3,
+            })}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {highlight}
+          </span>
+        )}
+        {after}
+        <span className="absolute -right-2 -top-3">
+          {emojis[MakeMeBeautiful.current]}
+        </span>
+      </p>
+    )
+  }
+
+  function handleAddJoke(e) {
+    e.preventDefault()
+    setJokes([...jokes, joke])
+    setJoke({ content: '', highlight: '' })
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center space-y-8 bg-gray-900 p-24">
+      <h1 className="relative mb-10 text-3xl font-bold">
+        The Notebook for{' '}
+        <span className="inline-block -rotate-2 bg-gray-100 p-2 text-black">
+          a chuckle
+        </span>
+        <span className="absolute -right-5 -top-5 rotate-12">🤣</span>
+      </h1>
+      <Image
+        src={Squirrel}
+        alt="squirrel laughing in front of laptop"
+        className="rounded-md"
+        width="400"
+      />
+
+      {jokes.map((individualJoke) => MakeMeBeautiful(individualJoke))}
+
+      <form className="space-y-3" onSubmit={handleAddJoke}>
+        <label htmlFor="joke" className="block">
+          Enter your joke
+        </label>
+        <input
+          id="joke"
+          type="text"
+          value={joke.content}
+          onChange={(e) => setJoke({ content: e.target.value, highlight: '' })}
+          className="rounded-md px-2 py-1 leading-8 text-black"
+        />
+        <button
+          type="submit"
+          className="ml-2 rounded-md bg-gray-200 px-6 py-2 font-medium text-black"
+        >
+          Add
+        </button>
+      </form>
+
+      <div>
+        <div>{joke.content ? 'Preview' : 'type something ...'}</div>
+        <div
+          className="relative rounded-md bg-slate-600 p-2"
+          onMouseUp={() =>
+            setJoke({
+              ...joke,
+              highlight: window.getSelection().toString(),
+            })
+          }
+        >
+          {joke.content && (
+            <span className="rounded-sm selection:bg-red-500">
+              {MakeMeBeautiful(joke)}
+            </span>
+          )}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
